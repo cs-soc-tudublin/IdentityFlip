0ceccba1c75dd45109365efd491b6a1eb4858627
Ruán <ruan@murgatroyd.me>
2024-06-30 13:49:37 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CS++ IdentityFlip
+
+IdentityFlip is CS++'s Universal Styles for web-based systems.
+
+It is based very closely on Redbrick's [Design System](https://github.com/redbrick/design-system)
---
+++ tailwind/index.js
@@ -0,0 +1,28 @@
+const themes = require('daisyui/src/theming/themes')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...themes.light,
+          primary: '#2a1191',
+          secondary: '#5d6ecd',
+          accent: '#f4c038',
+        },
+        dark: {
+          ...themes.dark,
+          primary: '#3f5af6',
+          secondary: '#bfd7ea',
+          accent: '#ffe61a',
+        },
+        synthwave: {
+          ...themes.synthwave,
+          primary: '#30306c',
+        },
+      },
+    ],
+  },
+}
